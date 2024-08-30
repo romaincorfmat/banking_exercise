75f32f44c52fd3f462e26bc15aa0cde5c1d1c222
serialfisher56 <112300122+noname20221@users.noreply.github.com>
2024-08-30 18:14:07 +0800

    try to fix progress component

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,3 +1,34 @@
+// "use client";
+
+// import * as React from "react";
+// import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+// import { cn } from "@/lib/utils";
+
+// const Progress = React.forwardRef<
+// 	React.ElementRef<typeof ProgressPrimitive.Root>,
+// 	React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+// >(({ indicatorClassName, className, value, ...props }, ref) => (
+// 	<ProgressPrimitive.Root
+// 		ref={ref}
+// 		className={cn(
+// 			"relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+// 			className,
+// 		)}
+// 		{...props}>
+// 		<ProgressPrimitive.Indicator
+// 			className={cn(
+// 				"h-full w-full flex-1 bg-primary transition-all",
+// 				indicatorClassName,
+// 			)}
+// 			style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+// 		/>
+// 	</ProgressPrimitive.Root>
+// ));
+// Progress.displayName = ProgressPrimitive.Root.displayName;
+
+// export { Progress };
+
 "use client";
 
 import * as React from "react";
@@ -8,7 +39,7 @@ import { cn } from "@/lib/utils";
 const Progress = React.forwardRef<
 	React.ElementRef<typeof ProgressPrimitive.Root>,
 	React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ indicatorClassName, className, value, ...props }, ref) => (
+>(({ indicatorclassname, className, value, ...props }, ref) => (
 	<ProgressPrimitive.Root
 		ref={ref}
 		className={cn(
@@ -19,7 +50,7 @@ const Progress = React.forwardRef<
 		<ProgressPrimitive.Indicator
 			className={cn(
 				"h-full w-full flex-1 bg-primary transition-all",
-				indicatorClassName,
+				indicatorclassname,
 			)}
 			style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
 		/>
